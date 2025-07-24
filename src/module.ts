eb76efe50df5554c7fe8cb32da38a872894f809d
warbam <eliott.dpw@gmail.com>
2025-07-24 21:56:39 +0200

    fix auto import and upgrade the README.md

--- src/module.ts
+++ src/module.ts
@@ -1,33 +1,38 @@
-import { defineNuxtModule, createResolver, addImportsDir, addPlugin } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  createResolver,
+  addImportsDir,
+  addPlugin,
+} from "@nuxt/kit";
 
 export default defineNuxtModule({
-	meta: {
-		name: 'laravel-rest-api-nuxt-sdk',
-		configKey: 'restApiSdk'
-	},
-	setup(_, nuxt) {
-		const resolver = createResolver(import.meta.url);
-		addImportsDir(resolver.resolve('./runtime/defineResource'));
-		addPlugin(resolver.resolve('./runtime/plugin'));
+  meta: {
+    name: "laravel-rest-api-nuxt-sdk",
+    configKey: "restApiSdk",
+  },
+  setup(_, nuxt) {
+    const resolver = createResolver(import.meta.url);
+    addImportsDir(resolver.resolve("./runtime/defineResource"));
+    addImportsDir(nuxt.options.srcDir + "/resources");
+    addPlugin(resolver.resolve("./runtime/plugin"));
 
-		nuxt.hook('prepare:types', (opts) => {
-			const typesDir = './runtime/types/'
-			if (!Array.isArray(opts.references)) {
-				opts.references = []
-			}
-			opts.references.push({
-				path: resolver.resolve(`${typesDir}restApiSdk.d.ts`)
-			})
-			opts.references.push({
-				path: resolver.resolve(`${typesDir}actions.d.ts`)
-			})
-			opts.references.push({
-				path: resolver.resolve(`${typesDir}search.d.ts`)
-			})
-			opts.references.push({
-				path: resolver.resolve(`${typesDir}mutate.d.ts`)
-			})
-		})
-		nuxt.options.imports.dirs?.push('./resources/**')
-	}
-})
+    nuxt.hook("prepare:types", (opts) => {
+      const typesDir = "./runtime/types/";
+      if (!Array.isArray(opts.references)) {
+        opts.references = [];
+      }
+      opts.references.push({
+        path: resolver.resolve(`${typesDir}restApiSdk.d.ts`),
+      });
+      opts.references.push({
+        path: resolver.resolve(`${typesDir}actions.d.ts`),
+      });
+      opts.references.push({
+        path: resolver.resolve(`${typesDir}search.d.ts`),
+      });
+      opts.references.push({
+        path: resolver.resolve(`${typesDir}mutate.d.ts`),
+      });
+    });
+  },
+});
